a6177ba47c4066dfaf217835787e25374234d3e5
zhangjing <1105249307@qq.com>
2019-03-03 15:15:19 +0800

    feat: use eui to build equipmentItem

---
+++ cardGame/src/PlayerScene.ts
@@ -0,0 +1,18 @@
+class PlayerScene extends eui.Component implements  eui.UIComponent {
+	
+	public constructor() {
+		super();
+	}
+
+	protected partAdded(partName:string,instance:any):void
+	{
+		super.partAdded(partName,instance);
+	}
+
+
+	protected childrenCreated():void
+	{
+		super.childrenCreated();
+	}
+	
+}
